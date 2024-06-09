dbe23c0ab7f03c90b76783cb406e5e923508d1d2
suborna12 <153237224+suborna12@users.noreply.github.com>
2024-06-09 20:07:14 +0600

    Add files via upload

---
+++ b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#define ll long long int
+#define nl cout << endl
+#define tc    \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define pb push_back
+#define all(x) x.begin(),x.end()
+using namespace std;
+void ss(){
+ll x,y;
+cin>>x>>y;
+yes;
+for(ll i=x;i<=y;i+=2){
+    cout<<i<<" "<<i+1<<endl;
+}
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t=1;
+    //cin>>t;
+    while(t--)
+    {
+        ss();
+    }
+}
